5c2df12ad6b25d7fbbdd11d7ced9b9f938584f36
lindl <13433836150.163.com>
2022-11-07 14:31:35 +0800

    sentinel整合

--- gateway/src/main/java/com/lindl/business/filter/AccessLimitGlobalFilter.java
+++ gateway/src/main/java/com/lindl/business/filter/AccessLimitGlobalFilter.java
@@ -20,7 +20,7 @@ import java.net.UnknownHostException;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
-@Component
+//@Component
 public class AccessLimitGlobalFilter implements GlobalFilter, Ordered {
 
     @Resource
